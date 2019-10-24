25f8a8976ccf2bda2149339e8c9db568f1e0a4bb
Nate Taylor <taylonr@gmail.com>
2019-10-23 20:17:28 -0500

    Removing placeholders

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,21 +1,16 @@
 import setText from './results.mjs';
 
 export function get(){
-    setText('A');
 }
 
 export function getCatch(){
-    setText('B');
 }
 
 export function chain(){
-    setText('C');
 }
 
 export function chainCatch(){
-    setText('C1')
 }
 
 export function final(){
-    setText('D');
 }
