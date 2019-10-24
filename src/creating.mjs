67bbfa4654387aba3e2c0815fa73a59822a66ec6
Nate Taylor <taylonr@gmail.com>
2019-10-23 20:45:33 -0500

    Starting to flesh out creating promises

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,22 +1,58 @@
-import setText from './results.mjs';
+import setText, { appendText } from "./results.mjs";
 
-export function timeout(){
+export function timeout() {
+  let wait = new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Timeout!");
+    }, 1500);
+  });
+
+  wait.then(setText);
 }
 
-export function interval(){
+export function interval() {
+  let counter = 0;
+  let wait = new Promise(resolve => {
+    setInterval(() => {
+      console.log("INTERVAL");
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+
+  wait.then(setText).finally(() => appendText(" -- Done"));
 }
 
-export function clearInterval(){
+export function clearIntervalChain() {
+  let counter = 0;
+  let interval;
+  let wait = new Promise(resolve => {
+    interval = setInterval(() => {
+      console.log("INTERVAL");
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+
+  wait
+    .then(setText)
+    .finally(() => {
+        clearInterval(interval)
+    });
 }
 
-export function xhr(){
+export function xhr() {
+  let request = new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+              xhr.open("GET", "http://localhost:3000/users");
+              xhr.onload = () => resolve(xhr.responseText);
+              xhr.onerror = () => reject(xhr.statusText);
+              xhr.send();
+  })
+
+  request.then(setText).catch(setText);
 }
 
-export function allPromises(){
-}
+export function allPromises() {}
 
-export function allSettled(){
-}
+export function allSettled() {}
 
-export function race(){
-}
+export function race() {}
