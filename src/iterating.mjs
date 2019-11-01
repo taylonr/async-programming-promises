2e79f6336179e7daff4f8a63f689877c3ad19ee0
Nate Taylor <taylonr@gmail.com>
2019-11-01 18:24:10 -0500

    Finishing up the iterating branch

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,39 +1,51 @@
 import setText, { appendText } from "./results.mjs";
 
-export async function get() {
-  const { data } = await axios.get("http://localhost:3000/orders/1");
+export async function get(){
+  const {data} = await axios.get("http://localhost:3000/orders/1");
   setText(JSON.stringify(data));
 }
 
-export async function getCatch() {
-  try {
-    const { data } = await axios.get("http://localhost:3000/orders/200");
-  } catch (error) {
+export async function getCatch(){
+  try{
+    const {data} = await axios.get("http://localhost:3000/orders/123");
+    setText(JSON.stringify(data));
+  } catch(error){
     setText(error);
   }
 }
 
-export async function chain() {
-  const { data: order } = await axios.get("http://localhost:3000/orders/1");
-  const { data } = await axios.get(
-    `http://localhost:3000/addresses/${order.shippingAddress}`
+export async function chain(){
+  const { data } = await axios.get("http://localhost:3000/orders/1");
+  const { data: address } = await axios.get(
+    `http://localhost:3000/addresses/${data.shippingAddress}`
   );
-
-  setText(JSON.stringify(data));
+  setText(`City: ${JSON.stringify(address.city)}`);
 }
 
-export async function concurrent() {
+export async function concurrent(){
   const orderStatuses = axios.get("http://localhost:3000/orderStatuses");
   const orders = axios.get("http://localhost:3000/orders");
 
   setText("");
 
   const { data: statuses } = await orderStatuses;
+  const { data: order } = await orders;
 
   appendText(JSON.stringify(statuses));
-
-  const { data: order } = await orders;
   appendText(JSON.stringify(order[0]));
+}
+
+export async function parallel(){
+  setText("");
+  await Promise.all([
+    (async () => {
+        const { data } = await axios.get("http://localhost:3000/orderStatuses");
+        appendText(JSON.stringify(data));
+      })(),
+    (async () => {
+      const { data } = await axios.get("http://localhost:3000/orders");
+      appendText(JSON.stringify(data[0]));
+    })()]);
 }
 
 export async function parallel() {
