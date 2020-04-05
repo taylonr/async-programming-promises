3347714e1b30f200eabdbd89e7898559182dd7e2
aminamos <26092352+aminamos@users.noreply.github.com>
2020-04-05 03:54:04 -0700

    catch and finally()

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -8,13 +8,66 @@ export function get() {
 }
 
 export function getCatch() {
+  axios.get("http://localhost:3000/orders/123")
+  .then(({data}) => {
+    setText(JSON.stringify(data))
+  })
+  .catch(error => setText(error))
 }
 
 export function chain() {
+  axios.get("http://localhost:3000/orders/1")
+  .then(({data}) => {
+    return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+  })
+  .then(({data}) => {
+    setText(`City: ${data.city}`)
+  })
 }
 
 export function chainCatch() {
+  // axios.get("http://localhost:3000/orders/1")
+  // .then(({data}) => {
+  //   axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+
+  //   throw new Error("Error")
+  // })
+  // .catch(err => {
+  //   setText(err);
+
+  //   throw new Error("Second error")
+  // })
+  // .then(({data}) => {
+  //   setText(`City: ${data.my.city}`)
+  // })
+  // .catch(err => setText(err))
+
+  axios.get("http://localhost:3000/orders/1")
+  .then(({data}) => {
+    return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+  })
+  .then(({data}) => {
+    setText(`City: ${data.city}`)
+  })
+  .catch(error => setText(error))
 }
 
 export function final() {
+  showWaiting()
+
+  axios.get("http://localhost:3000/orders/1")
+  .then(({data}) => {
+    return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+  })
+  .then(({data}) => {
+    setText(`City: ${data.city}`)
+  })
+  .catch(error => setText(error))
+  .finally(() => {
+    // setTimeout(() => {
+    //   hideWaiting()
+    // }, 1500);
+    hideWaiting()
+    appendText(" done")
+  })
 }
