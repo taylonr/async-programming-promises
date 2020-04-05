9c97dca644f5320f607ae5a3413603d65315c886
aminamos <26092352+aminamos@users.noreply.github.com>
2020-04-05 03:32:53 -0700

    get function

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,6 +1,10 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
+  axios.get("http://localhost:3000/orders/1")
+  .then(({data}) => {
+    setText(JSON.stringify(data))
+  })
 }
 
 export function getCatch() {
