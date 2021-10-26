ac921a11640139cefadfca80605a4d35fdb4a916
Aliiu <ayokunumi99@gmail.com>
2021-10-26 22:06:38 +0100

    first commit

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,6 +1,7 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
+    axios.get("http://localhost:3000/orders/1");
 }
 
 export function getCatch() {
