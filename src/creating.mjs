79da5145acd18138bcb6e03e5d4c512eadf55380
Fede Failla <fedefailla18@gmail.com>
2022-10-28 06:50:45 -0300

    4 - rejecting a promise

--- src/creating.mjs
+++ src/creating.mjs
@@ -37,7 +37,23 @@ export function clearIntervalChain() {
   wait.then((text) => setText(text)).finally(() => clearInterval(interval));
 }
 
-export function xhr() {}
+export function xhr() {
+  let wait = new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:3000/users/7");
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = () => reject("Request FAILED"); // this gets executed when there's an error or the url is incorrect
+    xhr.send();
+  });
+
+  wait.then((result) => setText(result)).catch((reason) => setText(reason));
+}
 
 export function allPromises() {}
 
