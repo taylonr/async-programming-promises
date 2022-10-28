37d6e8b48ddd9236f15a728f701f37bb4f9aa7a0
Fede Failla <fedefailla18@gmail.com>
2022-10-27 21:04:34 -0300

    4-timing out

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,22 +1,23 @@
 import setText, { appendText } from "./results.mjs";
 
 export function timeout() {
+  const wait = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Timeout");
+    }, 15000);
+  });
+  console.log("timing out!");
+  wait.then((text) => setText(text));
 }
 
-export function interval() {
-}
+export function interval() {}
 
-export function clearIntervalChain() {
-}
+export function clearIntervalChain() {}
 
-export function xhr() {
-}
+export function xhr() {}
 
-export function allPromises() {
-}
+export function allPromises() {}
 
-export function allSettled() {
-}
+export function allSettled() {}
 
-export function race() {
-}
+export function race() {}
